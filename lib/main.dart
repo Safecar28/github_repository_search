5512a86d9bdd4abcd0104b6b541a8bd25831a44c
SafeCar28 <nealnpatelthegreat@gmail.com>
2022-05-20 11:00:58 +0530

    WN added homepg to searchpg nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,65 @@
 import 'package:flutter/material.dart';
-// import 'package:github/github.dart';
 
 void main(List<String> args) => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//---------------------------- HomePage ------------------------------         =
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Github Repository Search',
-      home: Scaffold(
-          appBar: AppBar(
-        leading: const Icon(Icons.menu),
-        title: const Text('Famous Repositories'),
-        actions: const [
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+        actions: [
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Icon(Icons.search),
-          ),
-          Icon(Icons.more_vert),
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: ((context) => const SearchPage())));
+                },
+                icon: const Icon(Icons.search)),
+          )
         ],
-        backgroundColor: const Color.fromARGB(255, 156, 39, 176),
-      )),
+      ),
     );
   }
 }
 
-class Search extends StatefulWidget {
-  const Search({Key? key}) : super(key: key);
+//----------------------------- SearchPage -------------------------------     =
 
-  @override
-  State<Search> createState() => _SearchState();
-}
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
 
-class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Page'),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+      ),
+    );
+  }
+}
+
+//----------------------------- MyApp -------------------------------          =
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
   }
 }
